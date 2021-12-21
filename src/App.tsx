e1d0699e38c559e8d4975082e4a0172275eb225e
Artur Z <96476309+snrs-art@users.noreply.github.com>
2021-12-21 17:09:27 +0100

    something similar to `npx create-react-app@3.4.1 ds-boilerplate --template typescript --use-yarn`

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Hello Synerise Design System!</h1>
+      </div>
+    </>
+  );
+}
+export default App;
